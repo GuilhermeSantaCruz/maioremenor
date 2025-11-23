265e3d3347c83c971a595f3d335080e39abe59e4
Guilherme Lucas <guilhermelucaslsc@gmail.com>
2025-11-23 20:11:30 -0300

    linhas divisórias
    Acrescentei duas linhas divisórias

--- maioremenor/maioremenor.py
+++ maioremenor/maioremenor.py
@@ -9,9 +9,10 @@ for c in range(1, 6):
             menor = n
         if n > maior:
             maior = n
+print("**" * 20)            
 print(f"A lista completa: {numeros} ")
 print(f"O MENOR número da lista: {menor} ")
 print(f"O MAIOR número da lista: {maior} ")
-
+print("**" * 20)
 
 
